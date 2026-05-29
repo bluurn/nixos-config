eff126a9bd7225dd76e0a3c5da88b8350cd013f0
Vova <bluurn@gmail.com>
2026-05-29 22:27:12 +0200

    add home.nix to flake

--- flake.nix
+++ flake.nix
@@ -1,11 +1,11 @@
 {
-  description = "T480 NixOS Flake";
+  description = "NixOS T480 config";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -18,14 +18,14 @@
       inherit system;
 
       modules = [
-        ./hosts/t480/configuration.nix
+        ./configuration.nix
 
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
 
-          home-manager.users.vova = import ./home/vova.nix;
+          home-manager.users.vova = import ./home.nix;
         }
       ];
     };
