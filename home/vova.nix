1649c7a6b0ae5923de08b14e3aa5481e7da53faa
vova <vova@local>
2026-06-12 15:37:41 +0200

    add shell aliases

--- home/vova.nix
+++ home/vova.nix
@@ -13,6 +13,20 @@
 
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
+
+    shellAliases = {
+      n  = "nvim";
+      ll = "ls -lah";
+      la = "ls -A";
+      gs = "git status";
+      ga = "git add";
+      gc = "git commit";
+      gp = "git push";
+      gd = "git diff";
+
+      rebuild = "sudo nixos-rebuild switch --flake /etc/nixos#t480";
+      update = "cd /etc/nixos && nix flake update";
+    };
   };
 
   programs.starship = {
