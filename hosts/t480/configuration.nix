0141ffdc6601a8511bfa45ec0ac15ad8820b5e89
Vladimir Suvorov <bluurn@gmail.com>
2026-07-04 21:54:57 +0200

    factor out system module

--- hosts/t480/configuration.nix
+++ hosts/t480/configuration.nix
@@ -6,10 +6,10 @@
     ../../modules/gaming.nix
     ../../modules/desktop.nix
     ../../modules/development.nix
+    ../../modules/system.nix
     ../../modules/suspend.nix
   ];
 
-  boot.loader.systemd-boot.configurationLimit = lib.mkForce 5;
 
   users.users.vova = {
     isNormalUser = true;
@@ -19,16 +19,6 @@
     ];
     shell = pkgs.zsh;
   };
-
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
-
-  networking.networkmanager.enable = true;
-  networking.hostName = "t480";
-
-  time.timeZone = "Europe/Berlin";
 
   system.stateVersion = "25.11";
 }
---
+++ modules/system.nix
@@ -0,0 +1,12 @@
+_: {
+
+
+  boot.loader.systemd-boot.configurationLimit = lib.mkForce 5;
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+  networking.networkmanager.enable = true;
+  networking.hostName = "t480";
+  time.timeZone = "Europe/Berlin";
+  }
