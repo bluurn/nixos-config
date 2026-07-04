f8bd83c5d6db4900c7b55cf1f85c2e99819356d2
Vladimir Suvorov <bluurn@gmail.com>
2026-07-04 22:08:17 +0200

    extract shell

---
+++ home/shell.nix
@@ -0,0 +1,27 @@
+_: {
+  programs.zsh = {
+    enable = true;
+
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    shellAliases = {
+      n = "nvim";
+      ll = "ls -lah";
+      la = "ls -A";
+      gs = "git status";
+      ga = "git add";
+      gc = "git commit";
+      gp = "git push";
+      gd = "git diff";
+
+      rebuild = "sudo nixos-rebuild switch --flake /etc/nixos#t480";
+      update = "cd /etc/nixos && nix flake update";
+    };
+  };
+
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+}
--- home/vova.nix
+++ home/vova.nix
@@ -3,36 +3,11 @@
 {
   imports = [
     ./neovim.nix
+    ./shell.nix
   ];
   home.username = "vova";
   home.homeDirectory = "/home/vova";
   home.stateVersion = "25.11";
-
-  programs.zsh = {
-    enable = true;
-
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-
-    shellAliases = {
-      n = "nvim";
-      ll = "ls -lah";
-      la = "ls -A";
-      gs = "git status";
-      ga = "git add";
-      gc = "git commit";
-      gp = "git push";
-      gd = "git diff";
-
-      rebuild = "sudo nixos-rebuild switch --flake /etc/nixos#t480";
-      update = "cd /etc/nixos && nix flake update";
-    };
-  };
-
-  programs.starship = {
-    enable = true;
-    enableZshIntegration = true;
-  };
 
   programs.git = {
     enable = true;
