3645205f53c0c12e57892fbe9232517765c40f4c
Vladimir Suvorov <bluurn@gmail.com>
2026-07-04 22:13:50 +0200

    extract devtools

---
+++ home/devtools.nix
@@ -0,0 +1,29 @@
+_: {
+
+  programs.git = {
+    enable = true;
+    settings.user = {
+      name = "Vladimir Suvorov";
+      email = "bluurn@gmail.com";
+    };
+  };
+
+  programs.lazygit.enable = true;
+
+  programs.gpg = {
+    enable = true;
+  };
+
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+  };
+
+  services.gpg-agent = {
+    enable = true;
+    enableSshSupport = true;
+    pinentry = {
+      package = pkgs.pinentry-curses;
+    };
+  };
+}
--- home/vova.nix
+++ home/vova.nix
@@ -4,41 +4,17 @@
   imports = [
     ./neovim.nix
     ./shell.nix
+    ./devtools.nix
   ];
   home.username = "vova";
   home.homeDirectory = "/home/vova";
   home.stateVersion = "25.11";
-
-  programs.git = {
-    enable = true;
-    settings.user = {
-      name = "Vladimir Suvorov";
-      email = "bluurn@gmail.com";
-    };
-  };
-
-  programs.gpg = {
-    enable = true;
-  };
-
-  programs.direnv = {
-    enable = true;
-  };
-
-  services.gpg-agent = {
-    enable = true;
-    enableSshSupport = true;
-    pinentry = {
-      package = pkgs.pinentry-curses;
-    };
-  };
 
   home.packages = with pkgs; [
     tree
     ripgrep
     fd
     fzf
-    lazygit
     pass
   ];
 }
