68ee22959b3634c351d81a0b86c146c18f1ab0bb
Vladimir Suvorov <bluurn@gmail.com>
2026-07-04 20:52:33 +0200

    undo wayland stopgap fix

--- hosts/t480/configuration.nix
+++ hosts/t480/configuration.nix
@@ -25,7 +25,6 @@
 
   services.xserver.enable = true;
   services.displayManager.gdm.enable = true;
-  services.displayManager.gdm.wayland = false;
   services.desktopManager.gnome.enable = true;
 
   programs.firefox.enable = true;
