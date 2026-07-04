f234901ff4b7a114c1c811707547eaac21f85945
Vladimir Suvorov <bluurn@gmail.com>
2026-07-04 20:26:52 +0200

    disable psr

--- hosts/t480/configuration.nix
+++ hosts/t480/configuration.nix
@@ -5,6 +5,7 @@
     ./hardware-configuration.nix
     ../../modules/gaming.nix
     ../../modules/development.nix
+    ../../modules/suspend.nix
   ];
 
   users.users.vova = {
---
+++ modules/suspend.nix
@@ -0,0 +1,8 @@
+{ ... }:
+{
+  boot.kernelParams = [
+    "i915.enable_psr=0"
+  ];
+}
+
+
