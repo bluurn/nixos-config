5dfdf244258fc8a7fa93e4789495f86b6296ecc1
Vladimir Suvorov <bluurn@gmail.com>
2026-07-15 13:18:31 +0200

    force to use nix packages

--- home/nvim/lua/plugins/nix.lua
+++ home/nvim/lua/plugins/nix.lua
@@ -1,10 +1,43 @@
+local profile_bin = "/etc/profiles/per-user/" .. os.getenv("USER") .. "/bin/"
+
 return {
   {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
+        lua_ls = {
+          mason = false,
+          cmd = { profile_bin .. "lua-language-server" },
+          settings = {
+            Lua = {
+              workspace = {
+                checkThirdParty = false,
+              },
+              diagnostics = {
+                globals = { "vim" },
+              },
+            },
+          },
+        },
+
         nil_ls = {
           mason = false,
+          cmd = { profile_bin .. "nil" },
+        },
+
+        gopls = {
+          mason = false,
+          cmd = { profile_bin .. "gopls" },
+        },
+
+        marksman = {
+          mason = false,
+          cmd = { profile_bin .. "marksman" },
+        },
+
+        jsonls = {
+          mason = false,
+          cmd = { profile_bin .. "vscode-json-language-server", "--stdio" },
         },
       },
     },
